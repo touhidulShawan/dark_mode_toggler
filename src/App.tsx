fe2af6594fb88840e5099899b48a1a52ceb9745f
Touhidul Shawan <touhidulshawan@gmail.com>
2020-10-12 03:52:37 +0600

    complete the funcitonality

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,42 @@
 import * as React from "react";
+import { useState } from "react";
 import ToggleButton from "./components/ToggleButton";
 
+interface ContextProps {
+  dark: boolean;
+  toggleTheme: () => void;
+}
+
+export const ThemeContext = React.createContext<ContextProps>({
+  dark: false,
+  toggleTheme: function toggleTheme(): void {},
+});
+
 const App: React.FC = () => {
+  const [dark, setDark] = useState<boolean>(false);
+
+  const toggleTheme = () => {
+    setDark((prevState: boolean) => !prevState);
+  };
+
+  const theme: React.CSSProperties = {
+    height: "100vh",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: dark ? "#1A202C" : "#EDF2F7",
+    color: dark ? "#E2E8F0" : "#4A5568",
+  };
+
   return (
-    <main>
-      {/* toggler component  */}
-      <ToggleButton />
-      <h1>It`s a light theme</h1>
-    </main>
+    <ThemeContext.Provider value={{ dark, toggleTheme }}>
+      <main style={theme}>
+        {/* toggler component  */}
+        <ToggleButton />
+        {dark ? <h1>It`s a dark theme</h1> : <h1>it`s a light theme</h1>}
+      </main>
+    </ThemeContext.Provider>
   );
 };
 
--- src/components/ToggleButton.tsx
+++ src/components/ToggleButton.tsx
@@ -1,10 +1,14 @@
 import * as React from "react";
+import { useContext } from "react";
+import { ThemeContext } from "../App";
+import "./ToggleButton.css";
 
 const ToggleButton: React.FC = () => {
+  const theme = useContext(ThemeContext);
   return (
     <div>
       <label className="switch">
-        <input type="checkbox" />
+        <input type="checkbox" onChange={theme.toggleTheme} />
         <span className="slider round"></span>
       </label>
     </div>
